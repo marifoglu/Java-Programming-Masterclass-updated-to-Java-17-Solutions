0024cd0f5dfaab0a7bfd731c4c0440f9e6974cc9
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-05-23 20:16:29 +0100

    Next chapter packages added

--- src/main/java/Section_07_OOP_Part_1_Inheritance/CodingExercise36/Cylinder.java
+++ src/main/java/Section_07_OOP_Part_1_Inheritance/CodingExercise36/Cylinder.java
@@ -1,9 +1,7 @@
-package Section_07_OOP_Part_1_Inheritance.CodingExercise36;
+package src.main.java.Section_07_OOP_Part_1_Inheritance.CodingExercise36;
 
-public class Cylinder extends Circle {
-
+public class Cylinder extends Section_07_OOP_Part_1_Inheritance.CodingExercise36.Circle {
     private double height;
-
 
     public Cylinder(double radius, double height) {
         super(radius);
