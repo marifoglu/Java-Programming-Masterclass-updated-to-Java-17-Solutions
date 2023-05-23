cdf3c3b241a3ce3fd69f6a68dbbcc0b483f6f0f7
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-05-23 22:20:15 +0100

    CodingExercise38 added

---
+++ src/main/java/Section_07_OOP_Part_2_Polymophism/CodingExercise38/Ceiling.java
@@ -0,0 +1,19 @@
+package src.main.java.Section_07_OOP_Part_2_Polymophism.CodingExercise38;
+
+class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
---
+++ src/main/java/Section_07_OOP_Part_2_Polymophism/CodingExercise38/Wall.java
@@ -0,0 +1,13 @@
+package src.main.java.Section_07_OOP_Part_2_Polymophism.CodingExercise38;
+
+class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
