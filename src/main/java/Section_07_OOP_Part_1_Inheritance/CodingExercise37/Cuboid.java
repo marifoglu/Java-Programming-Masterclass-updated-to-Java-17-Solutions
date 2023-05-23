6e83e634ade6be2d0d2da80daa2b15c8cf97e3eb
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-05-23 19:38:32 +0100

    CodingExercise37 added

---
+++ src/main/java/Section_07_OOP_Part_1_Inheritance/CodingExercise37/Cuboid.java
@@ -0,0 +1,23 @@
+package src.main.java.Section_07_OOP_Part_1_Inheritance.CodingExercise37;
+
+public class Cuboid extends Rectangle{
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0){
+            this.height = 0;
+        }else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * super.getArea();
+    }
+}
---
+++ src/main/java/Section_07_OOP_Part_1_Inheritance/CodingExercise37/Rectangle.java
@@ -0,0 +1,33 @@
+package src.main.java.Section_07_OOP_Part_1_Inheritance.CodingExercise37;
+
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0) {
+            this.width = 0;
+        }else {
+            this.width = width;
+        }
+
+        if (length < 0) {
+            this.length = 0;
+        }else {
+            this.length = length;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+}
