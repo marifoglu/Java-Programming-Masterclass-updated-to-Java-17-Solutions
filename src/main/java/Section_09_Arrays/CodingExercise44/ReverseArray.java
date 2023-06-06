fb8daa6bac5cc024089e9a7c9e273f8e3fc4bf8f
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-06-06 16:07:38 +0100

    CodingExercise44 added

---
+++ src/main/java/Section_09_Arrays/CodingExercise44/ReverseArray.java
@@ -0,0 +1,38 @@
+package Section_09_Arrays.CodingExercise44;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    private static void reverse(int[] array) {
+        System.out.print("Array = [");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            // Swap elements at start and end indices
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.print("Reversed array = [");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
